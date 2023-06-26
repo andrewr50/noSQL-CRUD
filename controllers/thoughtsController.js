7c92dc0e425d34ba823e20d2253a4cd08ee3f633
andrewr50 <51382718+andrewr50@users.noreply.github.com>
2023-06-26 16:19:53 -0500

    moved Thought routes from server.js to thoughtController.js and made them functions

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,90 @@
+const { User, Thought } = require('../models')
+
+const thoughtController = {
+  // Creates a new thought
+  async createThought(req, res) {
+    try {
+      const newThought = await Thought.create(req.body);
+      //Update the thoughts array of the User that created the Thought to link them
+      const userData = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: {thoughts: newThought._id}},
+        { new: true }, // Returns updated document
+      )
+      
+      if (!userData) {
+        return res.status(404).json({message: "User not found"})
+      }
+
+      if (newThought) {
+        res.status(200).json(newThought);
+      }
+    } catch {
+      console.log('Something went wrong');
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  },
+
+  // Finds all thoughts
+  async getThoughts(req, res) {
+    try {
+      const result = await Thought.find({});
+      res.status(200).json(result);
+    } catch (err) {
+      console.log('Something went wrong');
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  },
+
+  // Finds Thought by id 
+  async findOneThought(req, res) {
+    try {
+      const result = await Thought.findOne({ _id: req.params.id });
+
+      if (!result) {
+        return res.status(404).json({message: 'No Thought with this id'});
+      }
+      res.status(200).json(result);
+    } catch (err) {
+      console.log('Something went wrong');
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  },
+
+  // Finds thought by id and deletes
+  async deleteThought(req, res) {
+    try {
+      const result = await Thought.findOneAndDelete({ _id: req.params.id });
+      res.status(200).json(result);
+
+      if (!result) {
+        res.status(404).json({ message: 'No Thought with this id'})
+      } else {
+      console.log(`Deleted: ${result}`);
+      }
+    } catch (err) {
+      console.log('Something went wrong');
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  },
+
+  // Finds Thought by id and updates
+  async updateThought(req, res) {
+    try {
+      const result = await Thought
+        .findOneAndUpdate(
+          { name: req.params.id }, // Finds Thought by id
+          { name: req.body.name }, // Updates name
+          { email: req.body.email}, // Updates email
+          { new: true } // Returns updated document
+        );
+      res.status(200).json(result);
+      console.log(`Updated: ${result}`);
+    } catch (err) {
+      console.log('Something went wrong');
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  }
+}
+
+module.exports = thoughtController;
---
+++ controllers/userController.js
@@ -0,0 +1,114 @@
+const { User } = require('../models')
+
+const userController = {
+  // Creates a new user
+  createUser(req, res) {
+    const newUser = 
+      new User({ 
+        username: req.body.username,
+        email: req.body.email,
+      });
+    newUser.save();
+    if (newUser) {
+      res.status(200).json(newUser);
+    } else {
+      console.log('Something went wrong');
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  },
+
+  // Finds all users
+  async getUsers(req, res) {
+    try {
+      // Using model in route to find all documents that are instances of that model
+      const result = await User.find({});
+      res.status(200).json(result);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  },
+
+  // Finds user by id
+  async getOneUser(req, res) {
+    try {
+      const result = await User.findOne({ _id: req.params.id })
+        .populate('thoughts') // Populate the thoughts array with created Thoughts
+        .populate('friends'); // Populate the friends array with friend ObjectId's
+      res.status(200).json(result);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  },
+
+  // Finds user by id and updates
+  async updateUser(req, res) {
+    try {
+      const result = await User
+        .findOneAndUpdate(
+          { name: req.params._id }, // Finds doc with id
+          { name: req.params.name }, // Updates name
+          { email: req.params.email}, // Updates email
+          { new: true } // Returns updated document
+        );
+      res.status(200).json(result);
+      console.log(`Updated: ${result}`);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  },
+
+  // Finds user by id and deletes
+  async deleteUser(req, res) {
+    try {
+      const result = await User.findOneAndDelete({ _id: req.params.id });
+      res.status(200).json(result);
+      console.log(`Deleted: ${result}`);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  },
+
+  // Add to User's friend list by User id
+  async addFriend(req, res) {
+    try {
+      const result = 
+        await User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $push: {friends: req.params.friendId}},
+          { new: true }, // Returns updated document
+        );
+
+      if (!result) {
+        return res.status(404).json({ message: 'No user with that Id' })
+      }
+    } catch (error) {
+      console.log('Something went wrong');
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  },
+
+  // Delete from User's friends list by id
+  deleteFriend (req, res) {
+    try {
+      User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: {friends: req.params.friendId}},
+        { new: true }, // Returns updated document
+      );
+
+      if (!result) {
+        return res.status(404).json({ message: 'No user with that Id' })
+      }
+
+    } catch (error) {
+          console.log('Something went wrong');
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  }
+};
+
+module.exports = userController;
